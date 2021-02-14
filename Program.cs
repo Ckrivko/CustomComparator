9bcbad32cdf721b7ef8c5dafb610c5c68b226e82
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2021-02-14 14:34:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            //Func<int, int, int> comparer =
+            //          (a, b) =>
+            //          {
+            //              if (a % 2 == 0 && b % 2 != 0)
+            //              {
+            //                  return -1;
+            //              }
+            //              else if (a % 2 != 0 && b % 2 == 0)
+            //              {
+            //                  return 1;
+            //              }
+
+            //              return a.CompareTo(b);
+            //          };
+
+            //Array.Sort(nums, comparer);// за оправяне!!
+
+            Array.Sort(nums, (a, b) =>
+            {
+                if (a % 2 == 0 && b % 2 != 0)
+                {
+                    return -1;
+                }
+                else if (a % 2 != 0 && b % 2 == 0)
+                {
+                    return 1;
+                }
+
+                return a.CompareTo(b);
+            });
+
+
+
+            Console.WriteLine(string.Join(" ",nums));
+        }
+
+        
+
+    }
+}
